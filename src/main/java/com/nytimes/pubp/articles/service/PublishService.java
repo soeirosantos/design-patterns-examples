90ed15d373cc919200ecd9fd645c53842d93f7a4
Romulo Santos <soeirosantos@gmail.com>
2019-02-13 16:43:50 -0500

    chain of responsibility - part II

--- src/main/java/com/nytimes/pubp/articles/service/PublishService.java
+++ src/main/java/com/nytimes/pubp/articles/service/PublishService.java
@@ -3,20 +3,17 @@ package com.nytimes.pubp.articles.service;
 import com.nytimes.pubp.articles.dao.ArticleDao;
 import com.nytimes.pubp.articles.dao.exception.DaoException;
 import com.nytimes.pubp.articles.domain.Article;
-import com.nytimes.pubp.articles.service.exception.InvalidArticleException;
 import com.nytimes.pubp.articles.service.exception.PublishException;
+import com.nytimes.pubp.articles.service.flow.PublishFlow;
+import com.nytimes.pubp.articles.service.flow.PublishStep;
+import com.nytimes.pubp.articles.service.flow.impl.PublishContext;
 import com.nytimes.pubp.audit.AuditService;
 import com.nytimes.pubp.audit.exception.AuditException;
-import com.nytimes.pubp.gateway.impl.GatewayClient;
 import com.nytimes.pubp.gateway.exception.GatewayClientException;
+import com.nytimes.pubp.gateway.impl.GatewayClient;
 import com.nytimes.pubp.notification.EmailNotificationService;
 import com.nytimes.pubp.notification.exception.NotificationException;
 import com.nytimes.pubp.security.SecurityContext;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 
 public class PublishService {
 
@@ -29,7 +26,7 @@ public class PublishService {
     private final EmailNotificationService notificationService;
 
     PublishService(ArticleDao articleDao, AuditService auditService, SecurityContext securityContext,
-                          GatewayClient gatewayClient, EmailNotificationService notificationService) {
+                   GatewayClient gatewayClient, EmailNotificationService notificationService) {
         this.articleDao = articleDao;
         this.auditService = auditService;
         this.securityContext = securityContext;
@@ -39,26 +36,9 @@ public class PublishService {
 
     public void publish(Article article) throws PublishException {
 
-        List<String> errors = new ArrayList<>();
-
-        if (article.getAuthor() == null || article.getAuthor().isBlank()) {
-            errors.add("Author cannot be empty");
-        }
-
-        if (article.getHeadline() == null || article.getHeadline().isBlank()) {
-            errors.add("Headline cannot be empty");
-        }
-
-        if (article.getPublishedAt() == null || article.getPublishedAt().isAfter(LocalDateTime.now())) {
-            errors.add("Publish date/time cannot be empty or a future date");
-        }
-
-        if (!errors.isEmpty()) {
-            throw new InvalidArticleException(errors);
-        }
-
-        article.setModifiedAt(LocalDateTime.now());
-        article.setUri(NYT_ARTICLE_SCHEME_PATH + UUID.randomUUID().toString());
+        PublishStep flow = PublishFlow.getFlow(ServiceType.PUBLISH);
+        PublishContext context = PublishContext.create(article);
+        flow.execute(context);
 
         try {
 
@@ -75,29 +55,10 @@ public class PublishService {
     }
 
     public void unpublish(Article article) throws PublishException {
-        List<String> errors = new ArrayList<>();
 
-        if (article.getAuthor() == null || article.getAuthor().isBlank()) {
-            errors.add("Author cannot be empty");
-        }
-
-        if (article.getHeadline() == null || article.getHeadline().isBlank()) {
-            errors.add("Headline cannot be empty");
-        }
-
-        if (article.getUnpublishedAt() == null || article.getUnpublishedAt().isAfter(LocalDateTime.now())) {
-            errors.add("Unpublish date/time cannot be empty or a future date");
-        }
-
-        if (article.getUri() == null || article.getUri().isBlank()) {
-            errors.add("URI cannot be empty");
-        }
-
-        if (!errors.isEmpty()) {
-            throw new InvalidArticleException(errors);
-        }
-
-        article.setModifiedAt(LocalDateTime.now());
+        PublishStep flow = PublishFlow.getFlow(ServiceType.UNPUBLISH);
+        PublishContext context = PublishContext.create(article);
+        flow.execute(context);
 
         try {
 
